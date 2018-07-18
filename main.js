e3add8ad66adc953d09a5ba158f9983d50038ecf
Karl Rodulfo <karlrodulfo@Karls-MacBook-Air.local>
2018-07-18 12:37:21 -0400

    ahhh

--- main.js
+++ main.js
@@ -8,40 +8,122 @@ function unclick() {
   document.getElementById('bodytag').style.backgroundColor = "";
 }
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
+function postPokemon1() {
+  var newcall = new XMLHttpRequest();
+  newcall.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var pokeinfo = JSON.parse(this.responseText);
+
+      document.getElementById("showname").innerHTML = "";
+      var pokename = document.createElement("h1");
+      var pokename1 = document.getElementById('showname')
+      pokename.textContent = pokeinfo.name;
+      pokename1.appendChild(pokename);
+
+      document.getElementById("showhp").innerHTML = "";
+      var pokehp = document.createElement('h3');
+      var pokehp1 = document.getElementById('showhp');
+      pokehp.textContent = " HP: " + pokeinfo.stats[5].base_stat;
+      pokehp1.appendChild(pokehp);
+
+      document.getElementById("showatk").innerHTML = "";
+      var pokeatk = document.createElement('h3');
+      var pokeatk1 = document.getElementById('showatk');
+      pokeatk.textContent = " ATTACK: " + pokeinfo.stats[4].base_stat;
+      pokeatk1.appendChild(pokeatk);
+
+      document.getElementById("showabilities").innerHTML = "";
+      var pokeabilities = document.createElement('h3');
+      var pokeabilities1 = document.getElementById('showabilities');
+      pokeabilities.textContent = "ABILITIES: " + pokeinfo.abilities[0].ability.name +", "+  pokeinfo.abilities[1].ability.name +", "+ pokeinfo.abilities[2].ability.name;
+      pokeabilities1.appendChild(pokeabilities);
+
+      document.getElementById("showimg").innerHTML = "";
+      var pokeimg = document.createElement('img');
+      var pokeimg1 = document.getElementById('showimg');
+      // pokeimg.src = pokeinfo.sprites.front_default;
+      pokeimg.src = "https://pre00.deviantart.net/512f/th/pre/f/2016/092/d/9/019___rattata_by_pr0xis0ul-d9xhuae.png"
+      pokeimg1.appendChild(pokeimg);
+    }
   }
-};
+  newcall.open("GET", "https://raw.githubusercontent.com/itskarl/pokemon-pokedex/master/rattata", true);
+  newcall.send();
+}
 
-xhttp.open("GET", "https://raw.githubusercontent.com/itskarl/pokemon-pokedex/master/rattata", true);
-xhttp.send();
+function postPokemon2() {
+  var newcall = new XMLHttpRequest();
+  newcall.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var pokeinfo = JSON.parse(this.responseText);
 
+      document.getElementById("showname").innerHTML = "";
+      var pokename = document.createElement("h1");
+      var pokename1 = document.getElementById('showname')
+      pokename.textContent = pokeinfo.name;
+      pokename1.appendChild(pokename);
 
-var pokeinfo = JSON.parse(this.responseText);
-console.log(pokeinfo.name)
+      document.getElementById("showhp").innerHTML = "";
+      var pokehp = document.createElement('h3');
+      var pokehp1 = document.getElementById('showhp');
+      pokehp.textContent = " HP: " + pokeinfo.stats[5].base_stat;
+      pokehp1.appendChild(pokehp);
 
+      document.getElementById("showatk").innerHTML = "";
+      var pokeatk = document.createElement('h3');
+      var pokeatk1 = document.getElementById('showatk');
+      pokeatk.textContent = " ATTACK: " + pokeinfo.stats[4].base_stat;
+      pokeatk1.appendChild(pokeatk);
 
-function postPokemon() {
-  //clear the content
-  document.getElementById('showpokemon').innerHTML = "";
+      document.getElementById("showabilities").innerHTML = "";
+      var pokeabilities = document.createElement('h3');
+      var pokeabilities1 = document.getElementById('showabilities');
+      pokeabilities.textContent = "ABILITIES: " + pokeinfo.abilities[0].ability.name +", "+  pokeinfo.abilities[1].ability.name +", "+ pokeinfo.abilities[2].ability.name;
+      pokeabilities1.appendChild(pokeabilities);
 
-  var pokename = document.createElement("h1");
-  var poke1 = document.getElementById('showpokemon')
-  pokename.textContent = "NAME";
-  poke1.appendChild(pokename);
-
-  var pokemon = document.createElement('img');
-  var pokebox = document.getElementById('showpokemon');
-  pokemon.src = "https://static.pokemonpets.com/images/monsters-images-800-800/19-Rattata.png";
-  pokebox.appendChild(pokemon);
-
-
+      document.getElementById("showimg").innerHTML = "";
+      var pokeimg = document.createElement('img');
+      var pokeimg1 = document.getElementById('showimg');
+      // pokeimg.src = pokeinfo.sprites.front_default;
+      pokeimg.src = "https://pre00.deviantart.net/512f/th/pre/f/2016/092/d/9/019___rattata_by_pr0xis0ul-d9xhuae.png"
+      pokeimg1.appendChild(pokeimg);
+    }
+  }
+  newcall.open("GET", "https://raw.githubusercontent.com/itskarl/pokemon-pokedex/master/rattata", true);
+  newcall.send();
 }
 
 
 
-// var em = document.createElement('em');
-// var para = document.querySelector(''); // reference an existing p element
-// em.textContent = 'Hello there!'; // give em some text content
-// para.appendChild(em); // embed em inside para
+
+
+
+
+var allPoke = [];
+
+class trainerKarl {
+  constructor(name) {
+    this.name = name;
+    this.all = function() {
+      allPoke.push(this.name)
+      console.log(allPoke);
+    }
+  }
+}
+
+class Pokemon extends trainerKarl {
+  constructor(name) {
+    super(name);
+    this.name = name;
+    this.health = 30;
+
+    var newcall = new XMLHttpRequest();
+    newcall.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var pokeinfo = JSON.parse(this.responseText);
+      }
+    }
+
+    newcall.open("GET", "https://raw.githubusercontent.com/itskarl/pokemon-pokedex/master/rattata", true);
+    newcall.send();
+  }
+}
