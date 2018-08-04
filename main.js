a6ab35cb1dfe14c99248b419c3b4fda1a01c96a9
Karl Rodulfo <karlrodulfo@gmail.com>
2018-08-04 12:16:29 -0400

    done

--- main.js
+++ main.js
@@ -147,12 +147,12 @@ function masterRelease() {
 
       setTimeout(function() {
 
-        document.getElementById("hpbar").style.width = ((pokeinfo.stats[5].base_stat / 255) * 100) + "%";
-        document.getElementById("atkbar").style.width = ((pokeinfo.stats[4].base_stat / 200) * 100) + "%";
-        document.getElementById("spatkbar").style.width = ((pokeinfo.stats[2].base_stat / 200) * 100) + "%";
-        document.getElementById("defbar").style.width = ((pokeinfo.stats[3].base_stat / 255) * 100) + "%";
-        document.getElementById("spdefbar").style.width = ((pokeinfo.stats[1].base_stat / 255) * 100) + "%";
-        document.getElementById("speedbar").style.width = ((pokeinfo.stats[0].base_stat / 200) * 100) + "%";
+        document.getElementById("hpbar").style.width = ((pokeinfo.stats[5].base_stat / 200) * 100) + "%";
+        document.getElementById("atkbar").style.width = ((pokeinfo.stats[4].base_stat / 181) * 100) + "%";
+        document.getElementById("spatkbar").style.width = ((pokeinfo.stats[2].base_stat / 173) * 100) + "%";
+        document.getElementById("defbar").style.width = ((pokeinfo.stats[3].base_stat / 200) * 100) + "%";
+        document.getElementById("spdefbar").style.width = ((pokeinfo.stats[1].base_stat / 200) * 100) + "%";
+        document.getElementById("speedbar").style.width = ((pokeinfo.stats[0].base_stat / 160) * 100) + "%";
 
       }, 1500);
 
